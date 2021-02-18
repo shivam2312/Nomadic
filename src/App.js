daf2a90e1129eedf788fbc7fba4639ebba484680
shivam2312 <shivamjaiswal2312@gmail.com>
2021-02-18 23:53:21 +0530

    React Ini

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import Users from './users/pages/Users'
+const App = () => {
+  return (
+    <Router>      
+      <Route path="/" exact ><Users/></Route>
+      <Redirect to="/"/>
+    </Router>
+    
+  );
+};
+
+export default App;
---
+++ src/users/pages/Users.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Users = ()=>{
+    return <h1>this is working</h1>;
+};
+
+export default Users;
